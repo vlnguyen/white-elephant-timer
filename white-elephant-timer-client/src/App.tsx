817a22677505476d74d14e257ccf1d1b616790c6
Vincent <vlnguyen@unomaha.edu>
2021-12-08 14:20:36 -0600

    install react-countdown

--- white-elephant-timer-client/src/App.tsx
+++ white-elephant-timer-client/src/App.tsx
@@ -1,9 +1,17 @@
+import Countdown, { CountdownRenderProps }  from 'react-countdown';
 import './App.css';
 
 function App() {
+  
+  function handleCountdownRender(props: CountdownRenderProps) {
+    const { minutes, seconds } = props.formatted;
+    return <div>{minutes}:{seconds}</div>;
+  }
+
   return (
     <div className="App">
       <div className="App-content">
+          <Countdown date={Date.now() + 60000} renderer={handleCountdownRender} />
       </div>
     </div>
   );
