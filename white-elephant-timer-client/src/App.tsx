95cfa5be7f89d7adc2f39cf460cec46da2c7fe93
Vincent <vlnguyen@unomaha.edu>
2021-12-08 14:24:00 -0600

    install moment

--- white-elephant-timer-client/src/App.tsx
+++ white-elephant-timer-client/src/App.tsx
@@ -1,17 +1,24 @@
-import Countdown, { CountdownRenderProps }  from 'react-countdown';
-import './App.css';
+import Countdown, { CountdownRenderProps } from "react-countdown";
+import moment from "moment";
+import "./App.css";
 
 function App() {
-  
   function handleCountdownRender(props: CountdownRenderProps) {
     const { minutes, seconds } = props.formatted;
-    return <div>{minutes}:{seconds}</div>;
+    return (
+      <div>
+        {minutes}:{seconds}
+      </div>
+    );
   }
 
   return (
     <div className="App">
       <div className="App-content">
-          <Countdown date={Date.now() + 60000} renderer={handleCountdownRender} />
+        <Countdown
+          date={moment().add(60, "seconds").toDate()}
+          renderer={handleCountdownRender}
+        />
       </div>
     </div>
   );
