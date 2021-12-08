c4391d15b4b8c96afaa2eff7acdcaf90e0644ffc
Vincent <vlnguyen@unomaha.edu>
2021-12-08 14:48:06 -0600

    add countdown controls

--- white-elephant-timer-client/src/App.tsx
+++ white-elephant-timer-client/src/App.tsx
@@ -1,13 +1,38 @@
 import Countdown, { CountdownRenderProps } from "react-countdown";
 import moment from "moment";
+import classNames from "classnames";
 import "./App.css";
 
 function App() {
   function handleCountdownRender(props: CountdownRenderProps) {
     const { minutes, seconds } = props.formatted;
+    const { pause, start, stop, isPaused, isStopped, isStarted, isCompleted } =
+      props.api;
+
+    const isActive =
+      isStarted() && !isCompleted() && (!isPaused() || !isStopped());
+
+    const playPauseStyle = classNames({
+      "App-button-play": !isActive,
+      "App-button-pause": isActive,
+    });
+
     return (
       <div>
         {minutes}:{seconds}
+        <div>
+          <div className="App-button-container">
+            <button
+              className={playPauseStyle}
+              onClick={() => (isActive ? pause() : start())}
+            >
+              {isActive ? "Pause" : "Start"}
+            </button>
+            <button className="App-button-stop" onClick={() => stop()}>
+              Stop
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -17,6 +42,7 @@ function App() {
       <div className="App-content">
         <Countdown
           date={moment().add(60, "seconds").toDate()}
+          autoStart={false}
           renderer={handleCountdownRender}
         />
       </div>
